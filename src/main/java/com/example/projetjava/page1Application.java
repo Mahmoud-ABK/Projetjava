95a6a0b11ae610bd173dbe321998a3860fa46fdd
Mohsen <mohsenallani2004@gmail.com>
2024-04-09 14:16:10 +0100

    Works fine

--- src/main/java/com/example/projetjava/page1Application.java
+++ src/main/java/com/example/projetjava/page1Application.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class page1Application extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(page1Application.class.getResource("test.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(page1Application.class.getResource("page1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
